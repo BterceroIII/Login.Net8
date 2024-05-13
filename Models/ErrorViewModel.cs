646e3e4bf2fcdc4eee850f91dbb1b7d096ab19f0
BterceroIII <b1tercero.cruz@gmail.com>
2024-05-12 19:51:28 -0600

    Delete file models

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AppLogin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
