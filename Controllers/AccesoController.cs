63e2cb122844c31997939eb585695652b6c1cc26
BterceroIII <b1tercero.cruz@gmail.com>
2024-05-18 22:31:26 -0600

    AccesoController: Create controller and view Registrarse

---
+++ Controllers/AccesoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Data;
+using Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppLogin.Controllers
+{
+    public class AccesoController : Controller
+    {
+        private readonly LoginDbContext _logincontext;
+
+        public AccesoController(LoginDbContext loginDbContext)
+        {
+            _logincontext = loginDbContext;
+        }
+
+        public IActionResult Registrarse()
+        {
+            return View();
+        }
+    }
+}
